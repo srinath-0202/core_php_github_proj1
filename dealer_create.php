6f3e1ed0fb8ed121545c1b46bfcab2e98fdc670a
srinath-0202 <srinath73025@gmail.com>
2025-11-13 16:57:26 +0530

    commiting dealer module file

---
+++ dealer_create.php
@@ -0,0 +1,41 @@
+<?php
+require 'db.php';
+
+
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+	$name = $_POST['name'];
+	
+	$sql = "insert into dealers(name,is_active,created_by,created_date) values ('$name',1,1,now())";
+	if($conn->query($sql) === true)
+	{
+		echo"Inserted Successfully";
+	} 
+	else
+	{
+		echo"Insertion Fail";
+	}
+
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Create Dealer</title>
+	<link rel="stylesheet" type="text/css" href="dealer_show_css.css">
+</head>
+<body>
+<header>
+	<h2>Create Dealer</h2>
+</header>
+<form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+	<label>Name : </label>
+	<input type="text" name="name">
+	<br><br>
+	<input type="submit" name="submit">
+</form>
+</body>
+</html>
--- dealer_show.php
+++ dealer_show.php
@@ -4,8 +4,12 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Dealers</title>
+	<link rel="stylesheet" type="text/css" href="dealer_show_css.css">
 </head>
 <body>
-
+<header>
+	<h2>Dealers View</h2>
+</header>
+<a href="dealer_create.php"><button>Add Dealer</button></a>
 </body>
 </html>
