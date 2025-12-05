70f052126d0eb84058d2889bfa12ce67e8c9e57f
srinath-0202 <srinath73025@gmail.com>
2025-12-05 15:20:30 +0530

    Dealer report done

--- dealer_show.php
+++ dealer_show.php
@@ -1,3 +1,9 @@
+<?php
+require 'db.php';
+
+$sql = "select * from dealers where is_active = 1";
+$result = $conn->query($sql); 
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,5 +17,25 @@
 	<h2>Dealers View</h2>
 </header>
 <a href="dealer_create.php"><button>Add Dealer</button></a>
+<table id="tb1"; border="1" >
+	<tr>
+		<th>Sno</th>
+		<th>Name</th>
+		<th>Email</th>
+		<th>Status</th>
+		<th>Created</th>
+		<th>Edit</th>
+	</tr>
+	<?php $i=1; while($row = $result->fetch_assoc()){ ?>
+		<tr>
+			<td><?php echo $i; ?></td>
+			<td><?php echo $row['name']; ?></td>
+			<td><?php echo $row['email']; ?></td>
+			<td><?php if($row['is_active'] == 1){ echo "Active"; } ?></td>
+			<td><?php echo $created_dt = date('Y-m-d', strtotime($row['created_date'])); ?></td>
+			<td><button>Edit</button></td>
+		</tr>
+	<?php $i++; } ?>
+</table>
 </body>
 </html>
